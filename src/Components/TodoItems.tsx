3d4527991c678001fd221a1fcb229cf8ce1bf0c3
unknown <nzerifova@gmail.com>
2025-01-30 16:52:21 +0400

    add  icons

--- src/Components/TodoItems.tsx
+++ src/Components/TodoItems.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { FaTrashCan } from "react-icons/fa6";
+import { MdEdit } from "react-icons/md";
 
 const Todoitems = () => {
   return (
     <li>
       content
+      <FaTrashCan />
+      <MdEdit />
     </li>
-  )
+  );
 }
 
 export default Todoitems
